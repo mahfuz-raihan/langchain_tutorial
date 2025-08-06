8af7324315e506828573455bffa80d0bce614827
mahfuz-raihan <mahfuz_raihan@outlook.com>
2025-08-07 00:26:44 +0600

    basic openai call

---
+++ 1. langchian_models/LLMs/1_llm_play.py
@@ -0,0 +1,12 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import os
+print("Loaded environment variables...")
+
+load_dotenv()
+
+llm = ChatOpenAI(model= "gpt-3.5-turbo-instruct")
+print("Initialized ChatOpenAI model...")
+resutl = llm.invoke("What is the capital of Bangladesh?")
+
+print("LLM response:", resutl)
