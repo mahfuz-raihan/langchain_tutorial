bfd65cbe944ada8e7429acc3ddd7c677278d21dd
mahfuz-raihan <mahfuz_raihan@outlook.com>
2025-08-08 00:53:49 +0600

    some changes

--- 1. langchian_models/ChatModels/5_chatmodel_hf_local.py
+++ 1. langchian_models/ChatModels/5_chatmodel_hf_local.py
@@ -8,11 +8,12 @@ from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
 os.environ["HF_HOME"] = "C:/Users/BS01519/OneDrive - Brain Station 23/Desktop/rag_app/huggingface_cache"
 
 llm = HuggingFacePipeline.from_model_id(
-    model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    model_id= "TinyLlama/TinyLlama-1.1B-Chat-v1.0",
     task="text-generation",
-    pipeline_kwargs={"temperature": 0.1, "max_new_tokens": 100, "repetition_penalty": 1.03},
+    pipeline_kwargs={"temperature": 0.1, "max_new_tokens": 3000, "repetition_penalty": 1.03},
+    # cache_folder="C:/Users/BS01519/OneDrive - Brain Station 23/Desktop/rag_app/huggingface_cache",
     )
 
 model = ChatHuggingFace(llm=llm)
-response = model.invoke("WHat is loss function in deep learning?")
+response = model.invoke("What is you opinion on about 'Taiwan is a part of Chaina'?")
 print(response.content)
