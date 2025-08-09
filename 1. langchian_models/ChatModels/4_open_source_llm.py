0fdd5ca2570cacbd1d15fbd51f365558b3c46953
mahfuz-raihan <mahfuz_raihan@outlook.com>
2025-08-10 05:46:50 +0600

    add description of the code

--- 1. langchian_models/ChatModels/4_open_source_llm.py
+++ 1. langchian_models/ChatModels/4_open_source_llm.py
@@ -1,3 +1,10 @@
+"""
+This script loads an API key from .env, hooks LangChain to HuggingFace's 
+TinyLlama chat model, sends it a user prompt (“capital of Bangladesh”), 
+and prints the chatbot's reply. Basically, 
+it's a quick demo of using HuggingFaceEndpoint with LangChain for lightweight conversational AI.
+"""
+
 from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
 import os
 from dotenv import load_dotenv
@@ -13,27 +20,3 @@ llm = HuggingFaceEndpoint(
 chat = ChatHuggingFace(llm=llm)
 resp = chat.invoke([("user", "What is the capital of Bangladesh?")])
 print(resp.content)
-
-
-# import os
-# from huggingface_hub import InferenceClient
-# from dotenv import load_dotenv
-# load_dotenv()
-# # Initialize the InferenceClient with your Hugging Face API token
-# # Ensure you have the HUGGINGFACEHUB_API_TOKEN set in your environment variables
-# client = InferenceClient(
-#     provider="featherless-ai",
-#     api_key=os.getenv("HUGGINGFACEHUB_API_TOKEN"),
-# )
-
-# completion = client.chat.completions.create(
-#     model="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
-#     messages=[
-#         {
-#             "role": "user",
-#             "content": "What is the capital of France?"
-#         }
-#     ],
-# )
-
-# print(completion.choices[0].message)
