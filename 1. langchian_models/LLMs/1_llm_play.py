27394ccd8933b437bd0717dfcea501c2560e814e
mahfuz-raihan <mahfuz_raihan@outlook.com>
2025-08-12 03:12:27 +0600

    update the llm models

--- 1. langchian_models/LLMs/1_llm_play.py
+++ 1. langchian_models/LLMs/1_llm_play.py
@@ -8,6 +8,16 @@ load_dotenv()
 llm = ChatOpenAI(model= "gpt-3.5-turbo-instruct")
 
 print("Initialized ChatOpenAI model...")
-resutl = llm.invoke("What is the capital of Bangladesh?")
+while True:
+    
+    user_input = input("Enter your question (or type 'exit' to quit): ")
+    if user_input.lower() == 'exit':
+        print("Exiting the program.")
+        break
+
+    print("Invoking LLM with user input...")
+    resutl = llm.invoke(user_input)
+    print("LLM response:", resutl)
+# resutl = llm.invoke("What is the capital of Bangladesh?")
 
 print("LLM response:", resutl)
