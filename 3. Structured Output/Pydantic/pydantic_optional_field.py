f84747d194eb3041812137e6118606faad1b9f84
mahfuz-raihan <mahfuz_raihan@outlook.com>
2025-08-14 00:42:11 +0600

    mod the example

--- 3. Structured Output/Pydantic/pydantic_optional_field.py
+++ 3. Structured Output/Pydantic/pydantic_optional_field.py
@@ -1,15 +1,18 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr 
 from typing import Optional
+import email_validator
 
 
 class Student(BaseModel):
     name: str
+    email: EmailStr
+    age: Optional[int]
 
 
 class Course(BaseModel):
     title: str = "Introduction to Programming"
     description: Optional[str] = None # Optional field with default value None
-    students: list[Student] = [{"name": "John Doe"}, {"name": "Jane Doe"}]
+    students: list[Student] = [{"name": "John Doe","email":"abc@gmail.com", "age":34}, {"name": "Jane Doe", "email":"trd"}]
 
 new_course = {}
 
