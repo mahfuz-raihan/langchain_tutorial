6ac9b8aa5375c452c79ea2592d15f1b361c863dc
mahfuz-raihan <mahfuz_raihan@outlook.com>
2025-08-18 02:16:44 +0600

    init (LangChain OutputParser): simple code example
    This code sample describe the manually code and context for parser

---
+++ 4. Output Parsers/1_StrOuputParser.py
@@ -0,0 +1,41 @@
+import os
+from dotenv import load_dotenv
+from huggingface_hub import login
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv(override=True)
+login(os.getenv("HUGGINGFACEHUB_API_TOKEN"))
+llm = HuggingFaceEndpoint(
+    repo_id= "deepseek-ai/DeepSeek-R1-0528", # "openai/gpt-oss-120b",
+    task="text-generation",
+    max_new_tokens=512,
+    do_sample=False,
+    repetition_penalty=1.03,
+    provider="auto",
+)
+chat_model = ChatHuggingFace(llm=llm)
+
+# 1st prompt -> detailed report
+template1 = PromptTemplate(
+    template="write a detailed report on {topic}",
+    input_variables=['topic']
+)
+# 2nd prompt -> summary 
+template2 = PromptTemplate(
+    template='Write a 5 line summary on the following text. \n {text}',
+    input_variables=['text']
+)
+
+
+# ---> Manual example 
+# invoke the prompt
+prompt1 = template1.invoke({'topic':"The China National Space Administration (CNSA)"})
+result1 = chat_model.invoke(prompt1)
+
+# invoke 2nd prompt
+prompt2 = template2.invoke({'text': result1.content})
+result2 = chat_model.invoke(prompt2)
+
+print(result2.content)
