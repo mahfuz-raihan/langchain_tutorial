d03a26271c5b87c70996e3885f5d336c45d5f3d1
mahfuz-raihan <mahfuz_raihan@outlook.com>
2025-08-18 03:35:47 +0600

    docs (StructutedOutputParser): data formating
    finding the output parser with structured way
    
    made it

---
+++ 4. Output Parsers/6_structutredoutputParser_v2.py
@@ -0,0 +1,40 @@
+import os
+from dotenv import load_dotenv
+from huggingface_hub import login
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain_core.prompts import PromptTemplate
+from langchain.output_parsers import StructuredOutputParser, ResponseSchema
+
+load_dotenv(override=True)
+login(os.getenv("HUGGINGFACEHUB_API_TOKEN"))
+print("Login successfully complete.........")
+
+
+llm = HuggingFaceEndpoint(
+    repo_id= "openai/gpt-oss-20b", #"deepseek-ai/DeepSeek-R1-0528", # "openai/gpt-oss-120b",
+    task="text-generation",
+    max_new_tokens=512,
+    do_sample=False,
+    repetition_penalty=1.03,
+    provider="auto",
+)
+chat_model = ChatHuggingFace(llm=llm)
+
+schema = [
+    ResponseSchema(name='fact_1', description='Fact 1 about the topic'),
+    ResponseSchema(name='fact_2', description='Fact 1 about the topic'),
+    ResponseSchema(name='fact_3', description='Fact 1 about the topic'),
+]
+
+parser = StructuredOutputParser.from_response_schemas(schema)
+
+template = PromptTemplate(
+    template='Give 3 fact about {topic} \n {format_instruction}',
+    input_variables=['topic'],
+    partial_variables={'format_instruction': parser.get_format_instructions()}
+)
+
+chain = template | chat_model | parser
+# prompt = template.invoke()
+final_result = chain.invoke({'topic':'black hole'})
+print(final_result)
