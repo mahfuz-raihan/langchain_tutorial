c65c116c27bafd917197db629bb4a6645410ba35
mahfuz-raihan <mahfuz_raihan@outlook.com>
2025-09-09 01:31:44 +0600

    feat: implement text loader and model processing with HuggingFace

---
+++ 7. Document Loaders/1_text_loader.py
@@ -0,0 +1,41 @@
+import os
+from dotenv import load_dotenv
+from huggingface_hub import login
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain.schema.runnable import RunnableParallel
+from langchain_community.document_loaders import TextLoader
+
+load_dotenv(override=True)
+login(os.getenv("HUGGINGFACEHUB_API_TOKEN"))
+print("Login successfully complete.........")
+
+
+loader = TextLoader(file_path='7. Document Loaders\\brag_doc.txt', encoding='utf-8')
+
+docs = loader.load()
+
+# model 1
+llm1 = HuggingFaceEndpoint(
+    repo_id= "openai/gpt-oss-20b", #"deepseek-ai/DeepSeek-R1-0528", # "openai/gpt-oss-120b",
+    task="text-generation",
+    max_new_tokens=512,
+    do_sample=False,
+    repetition_penalty=1.03,
+    provider="auto",
+)
+
+model = ChatHuggingFace(llm=llm1)
+
+prompt = PromptTemplate(
+    template='Write a summary of the {topic}',
+    input_variables=['topic']
+)
+parser = StrOutputParser()
+# print(docs[0])
+
+chain = prompt | model | parser
+
+result = chain.invoke({'topic': docs[0].page_content})
+print(result)
