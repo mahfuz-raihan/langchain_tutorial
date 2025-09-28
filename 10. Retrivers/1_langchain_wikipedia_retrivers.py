fd61bee06fe1a064b145c5a1c3d2e74ce28bbf88
mahfuz-raihan <mahfuz_raihan@outlook.com>
2025-09-28 06:54:54 +0600

    feat: add initial implementation of WikipediaRetriever for querying geopolitical history

---
+++ 10. Retrivers/1_langchain_wikipedia_retrivers.py
@@ -0,0 +1,16 @@
+from langchain_community.retrievers import WikipediaRetriever
+print("Necessary Librariy imported successfully.....")
+
+
+# initialize the retriverser 
+retrivers = WikipediaRetriever(top_k_results=2, lang='en')
+
+# define the query
+
+query = "the geopolitical history of Bangladesh and Pakistan from the perspective of a chinese."
+
+docs = retrivers.invoke(query)
+
+for i, doc in enumerate(docs):
+    print(f"\n --- Result {i+1} ---")
+    print(f"Content:\n{doc.page_content}...")
